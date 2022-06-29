67d6b26be2cf72c18d6787ff046628304c3ab990
whwu <whwu@appotronics.cn>
2022-06-29 18:36:08 +0800

    add Mcu Anf1 Anf2  Anf3. update to Light engine serial port communication protocol V0.5.docx.

--- AtProtocol/Src/At_HashTable/AtCmdHashTable.c
+++ AtProtocol/Src/At_HashTable/AtCmdHashTable.c
@@ -1,58 +1,48 @@
 #include "AtCmdHashTable.h"
 
-unsigned short g_atcmd_max_collide_num = 2;		/* AtCmd MAX Collide Num */
-unsigned short g_atcmd_num = 22;		/* AtCmd Num */
-unsigned short g_atcmd_htsize = 47;	/* AtCmd HashTable Size */
+unsigned short g_atcmd_max_collide_num = 3;		/* AtCmd MAX Collide Num */
+unsigned short g_atcmd_num = 17;		/* AtCmd Num */
+unsigned short g_atcmd_htsize = 37;	/* AtCmd HashTable Size */
 
 unsigned char g_atcmd_htsize_bits = 6;	/* AtCmd HashTable Size's bits */
 
 asCmdListUnit gsCmdList[] = {
-	{ 0x2b, NULL ,"Keystone2D"}, /* Keystone2D */
+	{ 0xf3, NULL ,"CwSpeed"}, /* CwSpeed */
+	{ 0xd4, NULL ,"LightSource"}, /* LightSource */
 	{ 0x0, NULL ,NULL},
+	{ 0xaf, NULL ,"Sn"}, /* Sn */
 	{ 0x0, NULL ,NULL},
-	{ 0x0, NULL ,NULL},
-	{ 0x42, NULL ,"SetCurrent"}, /* SetCurrent */
-	{ 0xd5, NULL ,"RunTime"}, /* RunTime */
+	{ 0xc0, NULL ,"SilentAsyncMessages"}, /* SilentAsyncMessages */
 	{ 0x0, NULL ,NULL},
 	{ 0x2b, NULL ,"System"}, /* System */
-	{ 0xbe, NULL ,"Eeprom"}, /* Eeprom */
-	{ 0x2a, NULL ,"VerticalKeystone"}, /* VerticalKeystone */
 	{ 0x0, NULL ,NULL},
-	{ 0x8e, NULL ,"GetCurrent"}, /* GetCurrent */
+	{ 0x1a, NULL ,"Ovp921"}, /* Ovp921 */
+	{ 0x0, NULL ,NULL},
+	{ 0x34, NULL ,"Temperature"}, /* Temperature */
 	{ 0xea, NULL ,"Version"}, /* Version */
 	{ 0x0, NULL ,NULL},
-	{ 0x46, NULL ,"Help"}, /* Help */
-	{ 0x57, NULL ,"Error"}, /* Error */
+	{ 0x0, NULL ,NULL},
+	{ 0x6a, NULL ,"Upgrade"}, /* Upgrade */
 	{ 0x0, NULL ,NULL},
 	{ 0x0, NULL ,NULL},
+	{ 0xbe, NULL ,"Eeprom"}, /* Eeprom */
 	{ 0x0, NULL ,NULL},
 	{ 0x0, NULL ,NULL},
-	{ 0x0, NULL ,NULL},
-	{ 0xd7, NULL ,"Gamma"}, /* Gamma */
 	{ 0xa7, NULL ,"TestPattern"}, /* TestPattern */
 	{ 0x0, NULL ,NULL},
 	{ 0x0, NULL ,NULL},
 	{ 0x0, NULL ,NULL},
+	{ 0x57, NULL ,"Error"}, /* Error */
 	{ 0x4c, NULL ,"LogInfo"}, /* LogInfo */
 	{ 0x0, NULL ,NULL},
-	{ 0x0, NULL ,NULL},
+	{ 0xe, NULL ,"Current"}, /* Current */
 	{ 0x0, NULL ,NULL},
 	{ 0x0, NULL ,NULL},
 	{ 0xd3, NULL ,"Reset"}, /* Reset */
 	{ 0x0, NULL ,NULL},
 	{ 0xea, NULL ,"InstallationMode"}, /* InstallationMode */
 	{ 0x0, NULL ,NULL},
-	{ 0x25, NULL ,"Warning"}, /* Warning */
-	{ 0x55, NULL ,"HorizonKeystone"}, /* HorizonKeystone */
-	{ 0xf3, NULL ,"CwSpeed"}, /* CwSpeed */
+	{ 0x0, NULL ,NULL},
 	{ 0x42, NULL ,"LightSourceTime"}, /* LightSourceTime */
-	{ 0x0, NULL ,NULL},
-	{ 0xd4, NULL ,"LightSource"}, /* LightSource */
-	{ 0x0, NULL ,NULL},
-	{ 0x34, NULL ,"Temperature"}, /* Temperature */
-	{ 0x0, NULL ,NULL},
-	{ 0x0, NULL ,NULL},
-	{ 0x0, NULL ,NULL},
-	{ 0xaf, NULL ,"Sn"}, /* Sn */
 };
 uint16_t gScmd_list_len = sizeof(gsCmdList) / sizeof(asCmdListUnit);
--- AtProtocol/Src/At_HashTable/AtKeyHashTable.c
+++ AtProtocol/Src/At_HashTable/AtKeyHashTable.c
@@ -1,45 +1,45 @@
 #include "AtKeyHashTable.h"
 
-unsigned short g_atkey_max_collide_num = 11;		/* AtKey Max Collide Num */
-unsigned short g_atkey_num = 63;		/* AtKey Num */
-unsigned short g_atkey_htsize = 127;	/* AtKey HashTable Size */
+unsigned short g_atkey_max_collide_num = 6;		/* AtKey Max Collide Num */
+unsigned short g_atkey_num = 51;		/* AtKey Num */
+unsigned short g_atkey_htsize = 103;	/* AtKey HashTable Size */
 
 unsigned char g_atkey_htsize_bits = 7;	/* AtKey HashTable Size's bits */
 
 asKeyListUnit gsKeyList[] = {
 	{ 0x42, 0 ,"B"}, /* B */
-	{ 0xef, 0 ,"BottomLeftX"}, /* BottomLeftX */
-	{ 0xf3, 0 ,"Black"}, /* Black */
-	{ 0xee, 0 ,"BottomLeftY"}, /* BottomLeftY */
-	{ 0xa1, 0 ,"Cyan"}, /* Cyan */
 	{ 0x47, 0 ,"G"}, /* G */
-	{ 0x34, 0 ,"DiagonalLines"}, /* DiagonalLines */
-	{ 0xec, 0 ,"McuSoftware"}, /* McuSoftware */
-	{ 0xbc, 0 ,"PlmSerial"}, /* PlmSerial */
-	{ 0xf1, 0 ,"NtcLcos"}, /* NtcLcos */
+	{ 0xf3, 0 ,"Black"}, /* Black */
+	{ 0x95, 0 ,"Idx"}, /* Idx */
+	{ 0xbf, 0 ,"Hour"}, /* Hour */
 	{ 0x52, 0 ,"R"}, /* R */
 	{ 0x59, 0 ,"Y"}, /* Y */
+	{ 0xe5, 0 ,"Off"}, /* Off */
 	{ 0x47, 0 ,"TableFront"}, /* TableFront */
-	{ 0xdb, 0 ,"Megenta"}, /* Megenta */
-	{ 0xd1, 0 ,"BottomRightX"}, /* BottomRightX */
-	{ 0xce, 0 ,"BottomRightY"}, /* BottomRightY */
+	{ 0x46, 0 ,"Checkerboard"}, /* Checkerboard */
+	{ 0xe4, 0 ,"Blue"}, /* Blue */
+	{ 0x9f, 0 ,"Ok"}, /* Ok */
+	{ 0xc2, 0 ,"NtcEnv"}, /* NtcEnv */
+	{ 0x2b, 0 ,"NtcGreen"}, /* NtcGreen */
+	{ 0xf1, 0 ,"NtcLcos"}, /* NtcLcos */
+	{ 0xab, 0 ,"NtcRed"}, /* NtcRed */
 	{ 0x0, 0 ,NULL},
 	{ 0xef, 0 ,"CeilingFront"}, /* CeilingFront */
+	{ 0x72, 0 ,"Red"}, /* Red */
+	{ 0x9c, 0 ,"On"}, /* On */
 	{ 0x0, 0 ,NULL},
 	{ 0x0, 0 ,NULL},
-	{ 0x2b, 0 ,"NtcGreen"}, /* NtcGreen */
-	{ 0x0, 0 ,NULL},
-	{ 0xc3, 0 ,"EngineVersion"}, /* EngineVersion */
-	{ 0x0, 0 ,NULL},
-	{ 0xac, 0 ,"White"}, /* White */
+	{ 0x81, 0 ,"Empty"}, /* Empty */
+	{ 0xf1, 0 ,"ScatteringWheel "}, /* ScatteringWheel  */
+	{ 0xa1, 0 ,"Cyan"}, /* Cyan */
+	{ 0x1a, 0 ,"Ovp921"}, /* Ovp921 */
+	{ 0x21, 0 ,"Size"}, /* Size */
+	{ 0x3b, 0 ,"Green"}, /* Green */
+	{ 0xce, 0 ,"Second"}, /* Second */
+	{ 0x67, 0 ,"TableRear"}, /* TableRear */
 	{ 0x80, 0 ,"Projector"}, /* Projector */
 	{ 0x0, 0 ,NULL},
-	{ 0x3b, 0 ,"Green"}, /* Green */
-	{ 0x4f, 0 ,"MachineSn"}, /* MachineSn */
-	{ 0x67, 0 ,"TableRear"}, /* TableRear */
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
+	{ 0xac, 0 ,"White"}, /* White */
 	{ 0x0, 0 ,NULL},
 	{ 0x14, 0 ,"CeilingRear"}, /* CeilingRear */
 	{ 0x0, 0 ,NULL},
@@ -48,49 +48,49 @@ asKeyListUnit gsKeyList[] = {
 	{ 0x3d, 0 ,"HorizontalRamp"}, /* HorizontalRamp */
 	{ 0x42, 0 ,"VerticalRamp"}, /* VerticalRamp */
 	{ 0x36, 0 ,"Grid"}, /* Grid */
+	{ 0xfb, 0 ,"Addr"}, /* Addr */
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
 	{ 0xc1, 0 ,"Minute"}, /* Minute */
 	{ 0x0, 0 ,NULL},
 	{ 0x0, 0 ,NULL},
+	{ 0xd0, 0 ,"Anf1"}, /* Anf1 */
 	{ 0x0, 0 ,NULL},
 	{ 0x0, 0 ,NULL},
-	{ 0x52, 0 ,"ColorBars"}, /* ColorBars */
-	{ 0x0, 0 ,NULL},
-	{ 0x53, 0 ,"SetBars"}, /* SetBars */
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
-	{ 0x36, 0 ,"NtcPowerSupply"}, /* NtcPowerSupply */
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
+	{ 0xd1, 0 ,"Anf2"}, /* Anf2 */
 	{ 0x0, 0 ,NULL},
 	{ 0x1c, 0 ,"LightEngine"}, /* LightEngine */
+	{ 0xd2, 0 ,"Anf3"}, /* Anf3 */
 	{ 0x0, 0 ,NULL},
 	{ 0x0, 0 ,NULL},
 	{ 0x0, 0 ,NULL},
-	{ 0x7c, 0 ,"ModelName"}, /* ModelName */
-	{ 0x0, 0 ,NULL},
+	{ 0x37, 0 ,"Mcu"}, /* Mcu */
 	{ 0x6, 0 ,"Factory"}, /* Factory */
+	{ 0x52, 0 ,"Data"}, /* Data */
 	{ 0xc3, 0 ,"User"}, /* User */
-	{ 0x47, 0 ,"HorizontalLines"}, /* HorizontalLines */
 	{ 0x0, 0 ,NULL},
-	{ 0xec, 0 ,"RGBRamps"}, /* RGBRamps */
+	{ 0x0, 0 ,NULL},
 	{ 0x53, 0 ,"All"}, /* All */
 	{ 0x0, 0 ,NULL},
 	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
-	{ 0x19, 0 ,"PartNumber"}, /* PartNumber */
+	{ 0x5d, 0 ,"Crc"}, /* Crc */
+	{ 0x73, 0 ,"NtcBlue"}, /* NtcBlue */
 	{ 0x1, 0 ,"Grey"}, /* Grey */
-	{ 0x86, 0 ,"TopRightX"}, /* TopRightX */
 	{ 0x0, 0 ,NULL},
-	{ 0x53, 0 ,"TopLeftX"}, /* TopLeftX */
-	{ 0xfb, 0 ,"Servicer"}, /* Servicer */
-	{ 0xea, 0 ,"InstallationMode"}, /* InstallationMode */
-	{ 0x52, 0 ,"TopLeftY"}, /* TopLeftY */
-	{ 0xbc, 0 ,"LcosSoftware"}, /* LcosSoftware */
 	{ 0x0, 0 ,NULL},
-	{ 0x85, 0 ,"TopRightY"}, /* TopRightY */
 	{ 0x0, 0 ,NULL},
-	{ 0xa1, 0 ,"Xpr"}, /* Xpr */
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
 	{ 0x0, 0 ,NULL},
 	{ 0x0, 0 ,NULL},
 	{ 0x0, 0 ,NULL},
@@ -102,37 +102,13 @@ asKeyListUnit gsKeyList[] = {
 	{ 0x28, 0 ,"Yellow"}, /* Yellow */
 	{ 0xa, 0 ,"SourceLight"}, /* SourceLight */
 	{ 0x0, 0 ,NULL},
-	{ 0x79, 0 ,"VerticalLines"}, /* VerticalLines */
+	{ 0x0, 0 ,NULL},
+	{ 0x22, 0 ,"Magenta"}, /* Magenta */
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
+	{ 0x0, 0 ,NULL},
 	{ 0x62, 0 ,"Status"}, /* Status */
-	{ 0x0, 0 ,NULL},
-	{ 0x72, 0 ,"NtcBLue"}, /* NtcBLue */
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
-	{ 0xbb, 0 ,"CheckErboard"}, /* CheckErboard */
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
-	{ 0x9f, 0 ,"Ok"}, /* Ok */
-	{ 0x0, 0 ,NULL},
-	{ 0x9c, 0 ,"On"}, /* On */
-	{ 0x0, 0 ,NULL},
-	{ 0xe5, 0 ,"Off"}, /* Off */
-	{ 0x0, 0 ,NULL},
-	{ 0x72, 0 ,"Red"}, /* Red */
-	{ 0xe4, 0 ,"Blue"}, /* Blue */
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
-	{ 0x24, 0 ,"BrandName"}, /* BrandName */
-	{ 0x0, 0 ,NULL},
-	{ 0xab, 0 ,"NtcRed"}, /* NtcRed */
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
-	{ 0x0, 0 ,NULL},
-	{ 0x81, 0 ,"Empty"}, /* Empty */
-	{ 0x0, 0 ,NULL},
 };
 uint16_t gSkey_list_len = sizeof(gsKeyList) / sizeof(asKeyListUnit);
